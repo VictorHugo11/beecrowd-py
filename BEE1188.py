3064b83a798e873b9ee8842cbe6b81ade2d96a35
Victor Hugoe <dev.victor011@gmail.com>
2025-04-06 12:39:20 -0300

    Área Inferior

---
+++ BEE1188.py
@@ -0,0 +1,21 @@
+c = input().upper()
+matriz = []
+
+
+for _ in range(12 * 12):
+    matriz.append(float(input()))
+
+soma = 0
+cont = 0
+
+for i in range(12):
+    for j in range(12):
+        if i > j and i + j > 11:
+            soma += matriz[i * 12 + j]
+            cont += 1
+            
+if c == "S":
+    print(f"{soma:.1f}")
+
+elif c == "M":
+    print(f"{soma / cont:.1f}")

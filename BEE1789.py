546a41bc9a4314bb26b2db09da978c0d117c8a11
Victor Hugoe <dev.victor011@gmail.com>
2025-04-11 20:37:31 -0300

    A Corrida de Lesmas

---
+++ BEE1789.py
@@ -0,0 +1,20 @@
+import sys
+
+while True:
+    try:
+        n = int(input())
+        a = list(map(int, input().split()))
+        maior = max(a)
+        
+        if maior < 10:
+            print("1")
+
+        if maior >= 10 and maior < 20:
+            print("2")
+
+        if maior >= 20:
+
+            print("3")
+
+    except EOFError:
+        break

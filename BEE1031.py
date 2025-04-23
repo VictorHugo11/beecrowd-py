e5091b6a71546c3f659cc72356d67e715205b8a2
Victor Hugoe <dev.victor011@gmail.com>
2025-04-22 21:36:42 -0300

    Crise de Energia v2

--- BEE1031.py
+++ BEE1031.py
@@ -1,20 +1,17 @@
-T = [[0 if i < 2 else -1 for _ in range(1001)] for i in range(101)]
-
 def josephus(n, k):
-    if T[n][k] == - 1:
-        T[n][k] = (josephus(n - 1, k) + k) % n
-    return T[n][k]
-
+    result = 0  
+    for i in range(2, n + 1):
+        result = (result + k) % i
+    return result
 
 while True:
     try:
         n = int(input())
-
         if n == 0:
             break
 
         m = 1
-        while((josephus(n - 1, m) + 1) % n != 12):
+        while ((josephus(n - 1, m) + 1) % n != 12):
             m += 1
 
         print(m)
